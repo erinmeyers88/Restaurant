0bfecbab479bdcf1e67bfbf7d56cc98bd794763b
Erin Vincent <evincent@agreserves.com>
2019-08-28 08:45:55 -0600

    Initial Commit

---
+++ Restaurant/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Restaurant
+//
+//  Created by Erin Vincent on 8/28/19.
+//  Copyright © 2019 Erin Vincent. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
